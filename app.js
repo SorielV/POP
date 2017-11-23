42bb95aade0849ee36440f32c6f354ab5c71d507
SorielV <xxsorelxx@gmail.com>
2017-11-22 23:41:07 -0600

    'App.js'

---
+++ app.js
@@ -0,0 +1,41 @@
+const app = require('./config/config'),
+  path = require('path'),
+  mongoose = require('mongoose'),
+  passport = require('passport');
+//
+mongoose.connect('mongodb://localhost/POPV2', { useMongoClient: true, promiseLibrary: global.Promise });
+mongoose.Promise = global.Promise;
+
+const indexRouter = require('./routes/index'),
+	usersRouter = require('./routes/users'),
+	projectRouter = require('./routes/project'),
+  apiProfile = require('./routes/api/profile'),
+  apiProject = require('./routes/api/project'),
+  apiMember = require('./routes/api/member'),
+  apiRequest = require('./routes/api/request'),
+	apiPost = require('./routes/api/post'),
+  apiComment = require('./routes/api/comment');
+  
+//api 
+app.use('/', indexRouter);
+
+const authorized = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    next(); 
+  } else {
+    res.redirect('/login');  
+  }
+};
+
+app.all('*', authorized);
+app
+  .use('/api/profile', apiProfile)
+  .use('/api/project', apiProject)
+  .use('/api/member', apiMember)
+  .use('/api/request', apiRequest)
+  .use('/api/post', apiPost)
+  .use('/api/comment', apiComment)
+  .use('/user', usersRouter)
+  .use('/project', projectRouter);
+
+app.listen(app.get('port'), () => console.log(`Server runing in port ${app.get('port')}`));
